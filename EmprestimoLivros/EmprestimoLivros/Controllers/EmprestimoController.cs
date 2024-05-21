9f2fb074bb66e1743d10dba7c10ff4aacd6ad0b9
Alexandre Direito <xandedsj@gmail.com>
2024-05-21 15:56:57 -0300

    passo 2 concluido

---
+++ EmprestimoLivros/EmprestimoLivros/Controllers/EmprestimoController.cs
@@ -0,0 +1,106 @@
+﻿using EmprestimoLivros.Data;
+using EmprestimoLivros.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmprestimoLivros.Controllers
+{
+    public class EmprestimoController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EmprestimoController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<EmprestimosModel> emprestimos = _context.Emprestimos;
+            return View(emprestimos);
+        }
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int? id)
+        {
+            if (id == null || id == 0) 
+            {
+                return NotFound();
+            }
+
+            EmprestimosModel emprestimo = _context.Emprestimos.FirstOrDefault(x => x.Id == id);
+            
+            if(emprestimo == null) 
+            {
+                return NotFound();
+            }
+
+            return View();  
+        }
+
+        [HttpGet]
+        public IActionResult Excluir(int? id) 
+        { 
+            if(id == null || id == 0) 
+            {
+                return NotFound();
+            }
+
+            EmprestimosModel emprestimo = _context.Emprestimos.FirstOrDefault(x => x.Id == id);
+       
+            if(emprestimo == null)
+            {
+                return NotFound();
+            }
+
+            return View();  
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(EmprestimosModel emprestimos)
+        {
+            if(ModelState.IsValid) 
+            {
+                _context.Emprestimos.Add(emprestimos);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Editar(EmprestimosModel emprestimos)
+        {
+            if (ModelState.IsValid) 
+            {
+                _context.Emprestimos.Update(emprestimos);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");   
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Excluir(EmprestimosModel emprestimo) 
+        { 
+            if(emprestimo == null)
+            {
+                return NotFound();
+            }
+
+            _context.Emprestimos.Remove(emprestimo);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ EmprestimoLivros/EmprestimoLivros/Migrations/20240521152206_CriacaoTabela.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmprestimoLivros.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriacaoTabela : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Emprestimos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Recebedor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Fornecedor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LivroEmprestado = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DataUltimaAtualizacao = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Emprestimos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Emprestimos");
+        }
+    }
+}
