2e764981585ccdea541bb69253f5bceedf4e5213
Alexandre Direito <xandedsj@gmail.com>
2024-05-21 13:11:32 -0300

    passo 1 concluído

---
+++ EmprestimoLivros/Controllers/EmprestimoController.cs
@@ -0,0 +1,22 @@
+﻿using EmprestimoLivros.Data;
+using EmprestimoLivros.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmprestimoLivros.Controllers
+{
+    public class EmprestimoController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EmprestimoController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<EmprestimosModel> emprestimos = _context.Emprestimos;
+            return View();
+        }
+    }
+}
